8afcdee53c6092b0f040164119be291b237eb704
holmat98 <62332336+holmat98@users.noreply.github.com>
2020-11-26 17:56:49 +0100

    Menu
    I added start menu and pause menu

---
+++ Assets/Script/PauseMenu2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu2 : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    public GameObject PauseMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+}
