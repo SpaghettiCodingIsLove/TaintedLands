9f4712a42e3d6d37e440d55f1cea0f4104bfbd16
holmat98 <62332336+holmat98@users.noreply.github.com>
2020-12-30 12:54:54 +0100

    citizens in village

--- Assets/Script/SkeletonAddingController.cs
+++ Assets/Script/SkeletonAddingController.cs
@@ -6,12 +6,24 @@ public class SkeletonAddingController : MonoBehaviour
 {
 
     public GameObject skeletonMonster;
+    public GameObject male;
+    public GameObject female;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i <= 50; i++)
+        for (int i = 0; i < 50; i++)
         {
             CreateSkeletonMonster();
+        }
+
+        for(int i = 0; i < 25; i++)
+        {
+            CreateMaleCitizen();
+        }
+
+        for(int i = 0; i < 25; i++)
+        {
+            CreateFemaleCitizen();
         }
     }
 
@@ -19,5 +31,17 @@ public class SkeletonAddingController : MonoBehaviour
     {
         Vector3 vector = new Vector3(Random.Range(1769.0f, 3100.0f), 170.0f, Random.Range(1411.0f, 3000.0f));
         Instantiate(skeletonMonster, vector, skeletonMonster.transform.rotation);
+    }
+
+    void CreateMaleCitizen()
+    {
+        Vector3 vector = new Vector3(Random.Range(2553.0f, 2785.0f), 100.0f, Random.Range(501.0f, 900.0f));
+        Instantiate(male, vector, male.transform.rotation);
+    }
+
+    void CreateFemaleCitizen()
+    {
+        Vector3 vector = new Vector3(Random.Range(2553.0f, 2785.0f), 100.0f, Random.Range(501.0f, 900.0f));
+        Instantiate(female, vector, female.transform.rotation);
     }
 }
