f2d18a294062ce06eb1163b12ed8e3db0d76aba7
Piwo13 <iworozycki@gmail.com>
2021-01-21 23:21:27 +0100

    Added weapon assets and weapon sheathing

---
+++ Assets/Script/WeaponOut.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponOut : MonoBehaviour
+{
+    GameObject sword;
+    string Animation1 = "GrabWeaponR";
+    string Animation2 = "PutAwayWeaponR";
+    Animator anim;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sword = GameObject.Find("Sword");
+        sword.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+
+    }   
+
+    void weaponOut()
+    {
+        sword.SetActive(true);
+    }
+
+    void weaponBack()
+    {
+        sword.SetActive(false);
+    }
+}
